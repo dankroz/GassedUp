8b5fa8bb01cc204f33712a91f43bfffb06e5609e
bbarikbin38 <bijan.barikbin38@gmail.com>
2019-05-18 14:06:17 -0400

    scraping

--- scrape.js
+++ scrape.js
@@ -7,30 +7,29 @@ const url = "https://gasprices.aaa.com/state-gas-price-averages/";
 axios.get(url)
     .then(response => {
         // console.log(response.data);
+        getData(response.data);
     })
     .catch(error => {
         console.log(error);
     })
 
 
-// Here, I am beginning the cheerio script for data parsing
+// Here, I am beginning the cheerio function for data parsing
 let getData = html => {
     data = [];
     const $ = cheerio.load(html);
     // looking through the sortable-table (or i think it is from the html)
-    $('table.sortable-table tr td').each((i, elem) => {
+    $(".sortable-table tr").each((i, elem) => {
         // we will need state, regular, mid_grade, premium, and diesel.  Those are the HTML classes from AAA's website
         // looks like the AAA website uses anchor tag for state names, then each gas price we can pull out by class name
         data.push({
-            // state: $(elem).find('td').text(),
-            regular: $(elem).find('td.regular').val()
-            // mid_grade: $(elem).text(),
-            // premium: $(elem).text(),
-            // diesel: $(elem).text()
-
+            state: $(elem).find('a').text().slice(3).trim(),
+            regular: $(elem).find('td.regular').text(),
+            mid_grade: $(elem).find('td.mid_grade').text(),
+            premium: $(elem).find('td.premium').text(),
+            diesel: $(elem).find('td.diesel').text()
         });
     });
     console.log(data);
 }
 
-getData("https://gasprices.aaa.com/state-gas-price-averages/");
