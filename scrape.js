879c0f61edf76aa8486f4ea286e631d8643bf0d9
nikki.zand <nzand54@gmail.com>
2019-05-22 19:57:13 -0400

    nikki env

--- scrape.js
+++ scrape.js
@@ -6,7 +6,7 @@ const url = "https://gasprices.aaa.com/state-gas-price-averages/";
 // getting the request to the AAA website using Axios for response data
 axios.get(url)
     .then(response => {
-        // console.log(response.data);
+        console.log(response.data);
     })
     .catch(error => {
         console.log(error);
@@ -15,14 +15,14 @@ axios.get(url)
 
 // Here, I am beginning the cheerio script for data parsing
 let getData = html => {
-    data = [];
+    let data = [];
     const $ = cheerio.load(html);
     // looking through the sortable-table (or i think it is from the html)
     $('table.sortable-table tr td').each((i, elem) => {
         // we will need state, regular, mid_grade, premium, and diesel.  Those are the HTML classes from AAA's website
         // looks like the AAA website uses anchor tag for state names, then each gas price we can pull out by class name
         data.push({
-            // state: $(elem).find('td').text(),
+            state: $(elem).find('td').text(),
             regular: $(elem).find('td.regular').val()
             // mid_grade: $(elem).text(),
             // premium: $(elem).text(),
@@ -30,7 +30,7 @@ let getData = html => {
 
         });
     });
-    console.log(data);
+    // console.log(response.data);
 }
 
 getData("https://gasprices.aaa.com/state-gas-price-averages/");
